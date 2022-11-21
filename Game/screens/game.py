1e6f107027d6b8019289b2b5c6a2414c27534220
Mitchel <heyitsmemitchel@gmail.com>
2022-11-21 14:55:21 -0800

    added started code for screens and gameloop

---
+++ Game/screens/game.py
@@ -0,0 +1,20 @@
+import random
+import pygame
+from screens import BaseScreen
+
+from ..components import Paddle, Ball, TileGroup
+from components import TextBox
+
+
+class GameScreen(BaseScreen):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def update(self):
+        pass
+        
+    def draw(self):
+        pass
+       
+    def manage_event(self, event):
+        pass
